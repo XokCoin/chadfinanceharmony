1e00f8a135238ab7049ce63e46e79c86a2d89547
josiah <josiahbrengle21@gmail.com>
2021-12-08 23:27:09 -0500

    1
    1

--- data/farms.data.ts
+++ data/farms.data.ts
@@ -18,6 +18,42 @@ const farmsdata: BasicInfo[] = [
         pid: 2,
         lpTokenAddress: "0x573dde4bfd787584408594c78dc56ebe6bb2fe1f",
         getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "1WBTC",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 3,
+        lpTokenAddress: "0xA54B4bEc71c92607cd4b795c1A45A29Bc9E7D607",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "Jewel",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 4,
+        lpTokenAddress: "0x1754d4e6267feae4ab5a4e68aa264bd70f82fd55",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "Kuro",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 5,
+        lpTokenAddress: "0x3500219C9bc6584000b1e4AF3c8f4006c86b44d6",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "Mis",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 6,
+        lpTokenAddress: "0x5474ed3e5ea7b74aae69c8d0a591916301459058",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
     }
 
 
