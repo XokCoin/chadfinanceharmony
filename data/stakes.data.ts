aea232291c356e287f9ceee8cc21add52b9b55f7
josiah <josiahbrengle21@gmail.com>
2021-12-11 01:56:29 -0500

    1
    1

--- data/stakes.data.ts
+++ data/stakes.data.ts
@@ -7,4 +7,58 @@ export const stakesdata = [
         lpTokenAddress: "0x6bAC77af0ceDD50356C90302993B67fea677570D",
         getLP: "https://game.defikingdoms.com/#/marketplace"
     },
+    {
+        first: "One-Chad",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 1,
+        lpTokenAddress: "0x9082E1b2996691E4c5434355ee1B73372f8f3519",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "UST-Chad",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 2,
+        lpTokenAddress: "0x573dde4bfd787584408594c78dc56ebe6bb2fe1f",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "1WBTC-Chad",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 3,
+        lpTokenAddress: "0xA54B4bEc71c92607cd4b795c1A45A29Bc9E7D607",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "Jewel-Chad",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 4,
+        lpTokenAddress: "0x1754d4e6267feae4ab5a4e68aa264bd70f82fd55",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "Kuro-Chad",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 5,
+        lpTokenAddress: "0x3500219C9bc6584000b1e4AF3c8f4006c86b44d6",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    },
+    {
+        first: "Mis-Chad",
+        second: "Chad",
+        approved: false,
+        earn: "Chad",
+        pid: 6,
+        lpTokenAddress: "0x5474ed3e5ea7b74aae69c8d0a591916301459058",
+        getLP: "https://game.defikingdoms.com/#/marketplace"
+    }
 ]
